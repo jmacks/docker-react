de1941b0ee342bc04bb7808f1245b609ed9f6d7c
Jeff Macks <jmacks1988@gmail.com>
2021-01-18 01:45:36 -0500

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/jmacks/i);
+  const linkElement = screen.getAllByText(/jmacks/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/jmacks/i);
+  const linkElement = screen.getAllByText(/jmacks/i);
   expect(linkElement).toBeInTheDocument();
 });
