0faf079d8bf2be4174db689aef4750a9140e3839
Jeff Macks <jmacks1988@gmail.com>
2021-01-18 01:56:34 -0500

    better fix the fuckin test already

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getAllByText(/jmacks/i);
-  expect(linkElement).toHaveTextContent('jmacks');
+  expect(linkElement[0]).toHaveTextContent('jmacks');
 });
